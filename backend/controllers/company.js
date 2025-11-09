8781bec249042d5247828b702b88b7156344320d
Chavaponsuu <chavapon1402@gmail.com>
2025-11-10 00:18:44 +0700

    add company controller

---
+++ backend/controllers/company.js
@@ -0,0 +1,21 @@
+const Company = require("../models/Company")
+exports.createCompany = async (req, res) => {
+  try {
+    const { name, address, website, description, tel } = req.body;
+    if (!name)
+      return res.status(400).json({ message: 'Company name is required' });
+
+    const company = await Company.create({
+      name,
+      address,
+      website,
+      description,
+      tel,
+    });
+
+    res.status(201).json(company);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
