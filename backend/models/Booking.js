9cc1fc72ef1a9376ad86c488bf25e1034476323d
Chavaponsuu <chavapon1402@gmail.com>
2025-11-10 00:19:16 +0700

    edit field

--- backend/models/Booking.js
+++ backend/models/Booking.js
@@ -4,7 +4,7 @@ const BookingSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   date: { type: Date, required: true },
   companies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Company', required: true }],
-  note: String
+
 }, { timestamps: true });
 
 module.exports = mongoose.model('Booking', BookingSchema);
